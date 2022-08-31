9db9a85ae48be54dc987d654d4de43ac30b29623
Luke Climenhage <luke.climenhage@gmail.com>
2022-08-31 14:26:07 -0400

    Added stylesheet to ViewImageScreen, cleaned up code

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { View, Image, Platform, StatusBar, Dimensions } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
     const screenSize = Dimensions.get("screen");
 
-    return <WelcomeScreen />;
+    return <ViewImageScreen />;
 }
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,51 +1,56 @@
 import React from "react";
-import { View, Image, Platform, StatusBar, Dimensions } from "react-native";
+import {
+    View,
+    Image,
+    Platform,
+    StatusBar,
+    Dimensions,
+    StyleSheet,
+} from "react-native";
 
 export default function App() {
-    const screenSize = Dimensions.get("screen");
-
     return (
-        <View
-            style={{
-                backgroundColor: "#000",
-                flex: 1,
-                flexDirection: "row",
-                justifyContent: "space-between",
-                paddingHorizontal: 30,
-                paddingVertical: 10,
-                paddingTop:
-                    Platform.OS === "android" ? StatusBar.currentHeight : 0,
-            }}
-        >
-            <View
-                style={{
-                    backgroundColor: "#fc5c65",
-                    height: 50,
-                    width: 50,
-                }}
-            ></View>
-            <View
-                style={{
-                    backgroundColor: "#4ecdc4",
-                    height: 50,
-                    width: 50,
-                }}
-            ></View>
+        <View style={styles.container}>
+            <View style={styles.closeIcon}></View>
+            <View style={styles.deleteIcon}></View>
 
             <Image
                 source={require("../assets/chair.jpg")}
-                style={{
-                    resizeMode: "contain",
-                    position: "absolute",
-                    flex: 1,
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    top: screenSize.height / 5,
-                    width: "100%",
-                    height: "100%",
-                }}
+                style={styles.image}
             />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    closeIcon: {
+        position: "absolute",
+        top: 40,
+        left: 30,
+        backgroundColor: "#fc5c65",
+        height: 50,
+        width: 50,
+    },
+    container: {
+        backgroundColor: "#000",
+        flex: 1,
+        // flexDirection: "row",
+        // justifyContent: "space-between",
+        // paddingHorizontal: 30,
+        // paddingVertical: 10,
+        // paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    },
+    deleteIcon: {
+        position: "absolute",
+        top: 40,
+        right: 30,
+        backgroundColor: "#4ecdc4",
+        height: 50,
+        width: 50,
+    },
+    image: {
+        resizeMode: "contain",
+        width: "100%",
+        height: "100%",
+    },
+});
