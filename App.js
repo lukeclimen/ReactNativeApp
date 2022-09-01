4740a894e9d7c7062e84c7beb518976c628da334
Luke Climenhage <luke.climenhage@gmail.com>
2022-09-01 19:31:47 -0400

    Created a messages screen

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import AppCard from "./app/components/AppCard";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import ListingDetailScreen from "./app/screens/ListingDetailScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-    return <WelcomeScreen />;
+    return <MessagesScreen />;
 }
